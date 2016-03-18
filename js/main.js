22fb1e39ecbb00d12f094942472f4f035cf79c6e
Shannon <shaw0106@algonquinlive.com>
2016-03-18 12:26:07 -0400

    added js but not working

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ $body.on('keydown', function () {
   var $ballAppear = $('<div>');
 
   $body.append($ballAppear);
+  $ballAppear.addClass('ball');
+  $ballAppear.css('left, Math.random()');
+  $ballAppear.css('top, Math.random()');
 });
 
-
-Math\.random\(\)
-\.css\('left')
+// Math.random()
+// \.css\('left')
