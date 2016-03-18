226910a8df6975773e3985e3768a6a18282b64c8
Shannon <shaw0106@algonquinlive.com>
2016-03-18 12:20:43 -0400

    css and html done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var $body = $('body');
+var $html = $('html');
+
+$body.on('keydown', function () {
+  var $ballAppear = $('<div>');
+
+  $body.append($ballAppear);
+});
+
+
+Math\.random\(\)
+\.css\('left')
